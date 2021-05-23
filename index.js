ca2e5255f626128ede8af5c254e4c43fe6fdb6a0
davidwpacker <davidwgpacker@gmail.com>
2021-05-23 18:52:16 +1000

    basic html and css setup

---
+++ index.js
@@ -0,0 +1,9 @@
+// initialise count = 0
+// listen for clicks (event listener): onclick event listener html attribute in html submit id
+// increment variable when button is clicked
+// change the count-el in the html file to reflect new count
+ 
+function increment() {
+    console.log("button was clicked");
+ }
+ 
