b1e817ac9772bcbde262b1818ea29db4528723ae
davidwpacker <davidwgpacker@gmail.com>
2021-05-24 21:05:28 +1000

    save button created and reset function inside it. Finalizing the base of this mini project.

--- index.js
+++ index.js
@@ -3,7 +3,30 @@
 // increment variable when button is clicked
 // change the count-el in the html file to reflect new count
  
+
+
+let saveEl = document.getElementById("save-el");    // grabs the save-el para. Stored in new variable. 
+let countEl = document.getElementById("count-el");
+let resetEl = document.getElementById("reset-btn");
+
+let count = 0;
+
+
 function increment() {
-    console.log("button was clicked");
- }
- 
+    count += 1;
+    countEl.textContent = count;
+}
+
+
+function save() {
+
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;   // .innerText here won't show the space/hidden/non-human readable content
+
+    countEl.textContent = 0;    // save button sets the h2 element to 0 (which is the text counting for us)
+    count = 0;      // must add this bc otherwise it keeps the value from before instead of counting up from 0 again. Must be reset!
+}
+
+
+
+
